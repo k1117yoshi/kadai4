fb80de39b217f072661453e1395c37327cabb84a
YOSHIKAWA Keisuke <z25064ky@aiit.ac.jp>
2025-06-06 16:20:55 +0000

    Edit "test.sh"

--- test.sh
+++ test.sh
@@ -12,23 +12,23 @@ ERROE_EXIT(){
 	exit 1
 }
 
-./script.sh 1> $tmp-ans && ERROR_EXIT "ERROE:引数が少ないのにエラーとならない"
+./script.sh 1 2> $tmp-ans && ERROR_EXIT "ERROE:引数が少ないのにエラーとならない"
 diff $tmp-ans $tmp-args-few || ERROE_EXIT "ERROR:引数が少ないエラー出力が不正"
 echo "引数が少ないクリア"
 
-./script.sh 1 2 3> $tmp-ans && ERROR_EXIT "ERROE:引数が多いのにエラーとならない"
+./script.sh 1 2 3 2> $tmp-ans && ERROR_EXIT "ERROE:引数が多いのにエラーとならない"
 diff $tmp-ans $tmp-args-many || ERROE_EXIT "ERROR:引数が多いエラー出力が不正"
 echo "引数が多いクリア"
 
-./script.sh hoge 1> $tmp-ans && ERROR_EXIT "ERROE:引数1が自然数ではないのにエラーとならない"
+./script.sh hoge 1 2> $tmp-ans && ERROR_EXIT "ERROE:引数1が自然数ではないのにエラーとならない"
 diff $tmp-ans $tmp-arg1-notnum || ERROE_EXIT "ERROR:引数1が自然数でないエラー出力が不正"
 echo "引数１が自然数でないクリア"
 
-./script.sh 1 -1> $tmp-ans && ERROR_EXIT "ERROE:引数2が自然数ではないのにエラーとならない"
+./script.sh 1 -1 2> $tmp-ans && ERROR_EXIT "ERROE:引数2が自然数ではないのにエラーとならない"
 diff $tmp-ans $tmp-arg2-notnum || ERROE_EXIT "ERROR:引数2が自然数でないエラー出力が不正"
 echo "引数２が自然数でないクリア"
 
-./script.sh 12 16> $tmp-ans || ERROR_EXIT "ERROE:正常に処理されない"
+./script.sh 12 16 2> $tmp-ans || ERROR_EXIT "ERROE:正常に処理されない"
 echo "正常系試験クリア"
 
 echo "END"
